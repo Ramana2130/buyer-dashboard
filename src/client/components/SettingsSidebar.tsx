f0a6a91e809b59ba6fbdf9b08ecb411e9ef3e47f
Ramana2130 <ramanaraj7143@gmailcom>
2024-07-14 10:51:27 +0530

    added

--- src/client/components/SettingsSidebar.tsx
+++ src/client/components/SettingsSidebar.tsx
@@ -1,27 +1,21 @@
-"use client"
-
-import { Link } from "react-router-dom"
-// import { usePathname } from "next/navigation"
-
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom'; // Importing Link and useLocation from react-router-dom
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
 
 interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
   items: {
-    href: string
-    title: string
-  }[]
+    href: string;
+    title: string;
+  }[];
 }
 
 export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
-//   const pathname = pathname()
+  const location = useLocation(); // useLocation hook from react-router-dom
 
   return (
     <nav
-      className={cn(
-        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
-        className
-      )}
+      className={cn('flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1', className)}
       {...props}
     >
       {items.map((item) => (
@@ -29,16 +23,16 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
           key={item.href}
           to={item.href}
           className={cn(
-            buttonVariants({ variant: "ghost" }),
-            // pathname === item.href
-            //   ? "bg-muted hover:bg-muted"
-            //   : "hover:bg-transparent hover:underline",
-            // "justify-start"
+            buttonVariants({ variant: 'ghost' }),
+            location.pathname === item.href // Compare location.pathname with item.href
+              ? 'bg-muted hover:bg-muted'
+              : 'hover:bg-transparent hover:underline',
+            'justify-start'
           )}
         >
           {item.title}
         </Link>
       ))}
     </nav>
-  )
+  );
 }
